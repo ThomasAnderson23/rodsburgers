1344b1bdc146516345e958f3746dba42e7aecc89
ThomasAnderson23 <renavarr@uc.cl>
2023-06-09 18:04:46 -0400

    Fixed Component 4

--- src/components/LittleMenu.js
+++ src/components/LittleMenu.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import Burger from "./Coffee"
+import Burger from "./Burger"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
 import Burger1 from "../images/Burger-item1.png"
