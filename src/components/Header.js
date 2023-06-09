f35e0b5aebd7c589ab708736744e0aea2cc768ad
ThomasAnderson23 <renavarr@uc.cl>
2023-06-09 18:05:57 -0400

    Edited UX Writing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <div className="d-flex justify-content-center align-items-start  flex-column ms-5 me-2 text-light">
         <h1>Homemade Burgers</h1>
-        <p className="paragraph-header">Flavorful creations grilled to perfection - Taste the love in every homemade burger! <br/> Go hard, go Rods!</p>
+        <p className="paragraph-header">Flavorful creations grilled to perfection - Taste real love in every homemade burger! <br/> Go hard, go Rods!</p>
         <Link>
           <Button variant="outline-light" size="lg">Enjoy now</Button>
         </Link>  
